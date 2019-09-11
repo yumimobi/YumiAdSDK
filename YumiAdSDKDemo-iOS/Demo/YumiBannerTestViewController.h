8981c91f6311709fbed5ef5e9b2465ec4fd696c5
ShunZhi Tang <tangshunzhi@zplay.com>
2019-09-11 16:59:29 +0800

    Replace new demo files

---
+++ YumiAdSDKDemo-iOS/Demo/YumiBannerTestViewController.h
@@ -0,0 +1,17 @@
+//
+//  YumiBannerTestViewController.h
+//  YumiMediationSDK-iOS_Example
+//
+//  Created by 王泽永 on 2019/8/26.
+//  Copyright © 2019 JiaDingYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YumiBannerTestViewController : UIViewController
+@property (nonatomic) NSString *placementID;
+@end
+
+NS_ASSUME_NONNULL_END
